36ee575dfad440b16aa5674ebe84cd42f3ec73eb
aman <aman.jangra@celebal.tech>
2022-11-30 18:03:09 +0530

    fix : new fixes applied

--- services/index.js
+++ services/index.js
@@ -3,3 +3,9 @@ console.log("service layer")
 console.log("changed")
 
 console.log("exptecting new release")
+
+console.log("service layer")
+
+console.log("changed")
+
+console.log("exptecting new release")
